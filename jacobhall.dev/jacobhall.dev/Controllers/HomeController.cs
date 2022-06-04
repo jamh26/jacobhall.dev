d50c42c5dc3cd4d76b47e0965a0fbd7f45548f28
jamh <jamh26@users.noreply.github.com>
2022-06-04 15:54:29 -0600

    feat: added new contact page and initial index layout

--- jacobhall.dev/jacobhall.dev/Controllers/HomeController.cs
+++ jacobhall.dev/jacobhall.dev/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace jacobhall.dev.Controllers
             return View();
         }
 
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
